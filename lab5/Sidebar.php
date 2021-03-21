bebbd886ecdd906edfb16893dfd1b6b8472e0816
Nahidul Isalm <60182620+reflection4046@users.noreply.github.com>
2021-03-21 13:11:05 +0600

    database

---
+++ lab5/Sidebar.php
@@ -0,0 +1,18 @@
+<head>
+    <style>
+    a:link 
+    {
+        text-decoration: none;
+    }
+   </style>
+</head>
+<td width="190" valign="top">
+            <ul>
+                <br><br><br>
+                <li><a href="addProduct.php" > Add Product</a></li>
+                <li><a href="searchProduct.php" > Search Product</a></li>
+                <li><a href="showAllProducts.php" > Show All Products</a></li>	
+            </ul>
+</td>
+<td valign="top">
+    
