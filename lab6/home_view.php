b20157f1ff0ba208d9927bfdc76567a5e6a9bedb
Nahidul Isalm <60182620+reflection4046@users.noreply.github.com>
2021-03-27 19:30:22 +0600

    Database...

---
+++ lab6/home_view.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head> <title> Home Page </title>
+	</head>   
+	 <body>
+		       <p> <h1 align="center">Hospital <h1> </p>
+		       <h3 align= "right">
+		       <a href="home_view.php">  Home |  </a> 
+		       <a href="login_view.php">  Login |  </a>  
+		       <a href="registration_view.php">  Registration </a> 
+		       <hr><br>
+	         <fieldset>
+		    <h1 align="center"> Welcome to Hospital </sub> </h1>  <br>
+		     <hr>
+		   <div align="center">
+		   <p> <span style="color: rgb(140, 140, 140);"> Copyright @  <?php echo date(2021);?> </span> </p>
+		    
+		   </div>
+		   </fieldset>
+	 </body>
+	 
+</html>	 
